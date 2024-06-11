a9045c1b53e69daf070304a3a5fa5e8a131c06fe
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-06-11 17:45:46 +0200

    Fix lint/formatting

--- packages/tracing-operations/src/utilities/errorMapper.ts
+++ packages/tracing-operations/src/utilities/errorMapper.ts
@@ -5,11 +5,10 @@ import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
 
 type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
 
-const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_BAD_REQUEST } = constants;
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_BAD_REQUEST } =
+  constants;
 
-export const operationsErrorMapper = (
-  error: ApiError<ErrorCodes>,
-): number =>
+export const operationsErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
     .with("tracingAlreadyExists", () => HTTP_STATUS_BAD_REQUEST)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
