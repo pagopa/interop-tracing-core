437bd1359d33d61a7a0db7800a470b1360381957
diego-lon <diego.longo@digitouch.it>
2024-06-12 14:21:41 +0200

    WIP: operation service test

--- packages/tracing-operations/test/operationsService.test.ts
+++ packages/tracing-operations/test/operationsService.test.ts
@@ -1,20 +1,56 @@
-import { initDB } from "pagopa-interop-tracing-commons";
+import { DB, initDB } from "pagopa-interop-tracing-commons";
 import { config } from "../src/utilities/config.js";
 import { PostgreSqlContainer } from "@testcontainers/postgresql";
-import { beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
 import {
   OperationsService,
   operationsServiceBuilder,
 } from "../src/services/operationsService.js";
 import { dbServiceBuilder } from "../src/services/db/dbService.js";
 import { resolve } from "path";
-import { PurposeId, TenantId, generateId } from "pagopa-interop-tracing-models";
-
+import {
+  CommonErrorCodes,
+  InternalError,
+  PurposeId,
+  TenantId,
+  generateId,
+  tracingState,
+} from "pagopa-interop-tracing-models";
+import { Wait } from "testcontainers";
+import {
+  addEservice,
+  addPurpose,
+  addTenant,
+  addTracing,
+  clearTestingData,
+  clearTracings,
+} from "./utils.js";
+import { Tracing } from "../src/model/domain/db.js";
+let dbInstance: DB;
 describe("Operations Service", () => {
   let operationsService: OperationsService;
+  const tenantIdId: TenantId = generateId();
+  const purposeId: PurposeId = generateId();
+  const eservice_id = generateId();
+  const todayTruncated = new Date().toISOString().split("T")[0];
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const yesterdayTruncated = yesterday.toISOString().split("T")[0];
 
   beforeEach(() => {
     vi.restoreAllMocks();
+  });
+
+  afterAll(() => {
+    clearTestingData(dbInstance);
   });
 
   beforeAll(async () => {
@@ -22,18 +58,23 @@ describe("Operations Service", () => {
       .withUsername(config.dbUsername)
       .withPassword(config.dbPassword)
       .withDatabase(config.dbName)
-      .withExposedPorts(5432)
+      .withExposedPorts(config.dbPort)
+      .withReuse()
       .withCopyFilesToContainer([
         {
           source: resolve(__dirname, "init-db.sql"),
           target: "/docker-entrypoint-initdb.d/01-init.sql",
         },
       ])
+      .withWaitStrategy(
+        Wait.forLogMessage("database system is ready to accept connections"),
+      )
       .start();
 
-    config.dbPort = postgreSqlContainer.getMappedPort(5432);
+    const dbPort = postgreSqlContainer.getMappedPort(5432);
+    console.log(`PostgreSQL is running on port ${dbPort}`);
 
-    const dbInstance = initDB({
+    dbInstance = initDB({
       username: config.dbUsername,
       password: config.dbPassword,
       host: config.dbHost,
@@ -44,22 +85,146 @@ describe("Operations Service", () => {
     });
 
     operationsService = operationsServiceBuilder(dbServiceBuilder(dbInstance));
+
+    await addEservice({ eservice_id, producer_id: generateId() }, dbInstance);
+
+    await addTenant(
+      {
+        id: tenantIdId,
+        name: "pagoPa",
+        origin: "external",
+        externalId: generateId(),
+        deleted: false,
+      },
+      dbInstance,
+    );
+
+    await addPurpose(
+      {
+        id: purposeId,
+        consumer_id: tenantIdId,
+        eservice_id,
+        purpose_title: "purpose_for_tenant",
+      },
+      dbInstance,
+    );
   });
 
-  describe("Tenants service", () => {
-    describe("getTenantByPurposeId", () => {
-      it("retrieve tenant by purposeId", async () => {
-        const tenantIdId: TenantId = generateId();
-        const purposeId: PurposeId = generateId();
+  describe("getTenantByPurposeId", () => {
+    it("retrieve tenant by purposeId", async () => {
+      const result = await operationsService.getTenantByPurposeId(purposeId);
 
-        vi.spyOn(operationsService, "getTenantByPurposeId").mockResolvedValue(
-          tenantIdId,
+      expect(result).toBe(tenantIdId);
+    });
+    it("should give error when purposeId not a tenant", async () => {
+      const wrongPurposeId: PurposeId = generateId();
+
+      try {
+        await operationsService.getTenantByPurposeId(wrongPurposeId);
+      } catch (e) {
+        const error = e as InternalError<CommonErrorCodes>;
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toContain(
+          "Error getTenantByPurposeId: QueryResultError",
         );
+        expect(error.code).toBe("genericError");
+      }
+    });
+  });
 
-        const result = await operationsService.getTenantByPurposeId(purposeId);
+  describe("submitTracing", () => {
+    it("should insert a tracing with errors to be false", async () => {
+      const tracing = {
+        tenantId: tenantIdId as string,
+        date: todayTruncated,
+      };
+      const result = await operationsService.submitTracing(tracing);
+      await clearTracings(dbInstance);
+      expect(result).toHaveProperty("tracingId");
+      expect(result.tenantId).toBe(tenantIdId);
+      expect(result.errors).toBe(false);
+      expect(result.state).toBe("PENDING");
+      expect(result.version).toBe(1);
+    });
+    it("should insert a tracing with errors to be true", async () => {
+      const tracing = {
+        tenantId: tenantIdId as string,
+        date: todayTruncated,
+      };
+      const tracingMissing: Tracing = {
+        id: generateId() as string,
+        tenant_id: tenantIdId as string,
+        state: tracingState.missing,
+        date: yesterdayTruncated,
+        version: 1,
+        errors: true,
+      };
+      await addTracing(tracingMissing, dbInstance);
+      const result = await operationsService.submitTracing(tracing);
+      await clearTracings(dbInstance);
+      expect(result).toHaveProperty("tracingId");
+      expect(result.tenantId).toBe(tenantIdId);
+      expect(result.errors).toBe(true);
+      expect(result.state).toBe("PENDING");
+      expect(result.version).toBe(1);
+    });
+    it("should update a tracing when state is missing", async () => {
+      const tracingMissing: Tracing = {
+        id: generateId() as string,
+        tenant_id: tenantIdId as string,
+        state: tracingState.missing,
+        date: todayTruncated,
+        version: 1,
+        errors: true,
+      };
+      await addTracing(tracingMissing, dbInstance);
+      const tracing = {
+        tenantId: tenantIdId as string,
+        date: todayTruncated,
+      };
+      const result = await operationsService.submitTracing(tracing);
+      const expectedDate = new Date(tracing.date);
+      expect(result).toHaveProperty("tracingId");
+      expect(result.tenantId).toBe(tenantIdId);
+      expect(result.errors).toBe(false);
+      expect(result.state).toBe("PENDING");
+      result.date && expect(new Date(result.date)).toEqual(expectedDate);
+      expect(result.version).toBe(1);
+      expect(1).toBe(1);
+    });
+    it("should give error if tenant submits a tracing for the same day when status is not error or missing", async () => {
+      const existingTracing = {
+        id: generateId() as string,
+        tenant_id: tenantIdId as string,
+        state: tracingState.completed,
+        date: todayTruncated,
+        version: 1,
+        errors: false,
+      };
+      await addTracing(existingTracing, dbInstance);
 
-        expect(result).toBe(tenantIdId);
-      });
+      const tracing = {
+        tenantId: tenantIdId as string,
+        date: todayTruncated,
+      };
+      try {
+        await operationsService.submitTracing(tracing);
+      } catch (e) {
+        const error = e as InternalError<CommonErrorCodes>;
+        expect(error).toBeInstanceOf(Error);
+        expect(error.code).toBe("tracingAlreadyExists");
+      }
+    });
+    it("should give error if there's an error during database operations", async () => {
+      const mockDb = vi
+        .spyOn(dbInstance, "one")
+        .mockRejectedValueOnce(new Error("Database error"));
+      const tracing = {
+        tenantId: tenantIdId as string,
+        date: todayTruncated,
+      };
+      await expect(operationsService.submitTracing(tracing)).rejects.toThrow();
+      mockDb.mockRestore();
     });
   });
 });
---
+++ packages/tracing-operations/test/utils.ts
@@ -0,0 +1,88 @@
+import { DB } from "pagopa-interop-tracing-commons";
+import {
+  Purpose,
+  PurposeError,
+  Tenant,
+  Tracing,
+} from "../src/model/domain/db.js";
+import { DateUnit, truncatedTo } from "../src/utilities/date.js";
+
+export async function addPurpose(purposeValues: Purpose, db: DB) {
+  const insertPurposeQuery = `
+      INSERT INTO tracing.purposes (id, consumer_id, eservice_id, purpose_title)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id
+    `;
+
+  return await db.one(insertPurposeQuery, Object.values(purposeValues));
+}
+
+export async function addTenant(tenantValues: Tenant, db: DB) {
+  const insertTenantQuery = `
+      INSERT INTO tracing.tenants (id, name, origin, external_id, deleted)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING id
+    `;
+
+  return await db.one(insertTenantQuery, Object.values(tenantValues));
+}
+
+export async function addPurposeError(
+  purposeErrorValues: PurposeError,
+  db: DB,
+) {
+  const insertPurposeErrorQuery = `
+      INSERT INTO tracing.purpose_errors (id, tracing_id, version, purpose_id, error_code, message)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `;
+
+  await db.one(insertPurposeErrorQuery, Object.values(purposeErrorValues));
+}
+
+export async function addTracing(tracingValues: Tracing, db: DB) {
+  const truncatedDate: Date = truncatedTo(
+    new Date(tracingValues.date).toISOString(),
+    DateUnit.DAYS,
+  );
+  const insertTracingQuery = `
+      INSERT INTO tracing.tracings (id, tenant_id, state, date, version, errors)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `;
+  await db.one(insertTracingQuery, [
+    tracingValues.id,
+    tracingValues.tenant_id,
+    tracingValues.state,
+    truncatedDate,
+    tracingValues.version,
+    tracingValues.errors,
+  ]);
+}
+
+export async function addEservice(
+  eServiceValues: { eservice_id: string; producer_id: string },
+  db: DB,
+) {
+  const insertEserviceQuery = `
+  INSERT INTO tracing.eservices (eservice_id, producer_id)
+  VALUES ($1, $2)
+  RETURNING eservice_id`;
+  await db.one(insertEserviceQuery, Object.values(eServiceValues));
+}
+
+export async function clearTestingData(db: DB) {
+  const deleteTestingDataQuery = `
+  DELETE FROM tracing.purposes;
+  DELETE FROM tracing.eservices;
+  DELETE FROM tracing.tracings;
+  DELETE FROM tracing.tenants;
+  `;
+  await db.any(deleteTestingDataQuery);
+}
+export async function clearTracings(db: DB) {
+  const deleteTracingsQuery = `
+  DELETE FROM tracing.tracings;
+  `;
+  await db.any(deleteTracingsQuery);
+}
