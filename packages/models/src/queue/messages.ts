2b7fcf2060409c274cea5d667ff4c6bd0941ed2a
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-06-28 14:19:20 +0200

    Update models

---
+++ packages/models/src/queue/messages.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { TracingState } from "../tracing/tracing.js";
+
+export const UpdateTracingStateDto = z.object({
+  tracingId: z.string().uuid(),
+  state: TracingState,
+  version: z.number(),
+});
+
+export type UpdateTracingStateDto = z.infer<typeof UpdateTracingStateDto>;
+
+export const SavePurposeErrorDto = z.object({
+  tracingId: z.string().uuid(),
+  purposeId: z.string().uuid(),
+  version: z.number(),
+  date: z.string(),
+  errorCode: z.string(),
+  message: z.string(),
+  rowNumber: z.number(),
+  updateTracingState: z.boolean(),
+});
+
+export type SavePurposeErrorDto = z.infer<typeof SavePurposeErrorDto>;
