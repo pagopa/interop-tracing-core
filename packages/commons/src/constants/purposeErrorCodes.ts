fbcf90e196b1f889a43ec11bd5e88860c692790f
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-06-28 14:19:30 +0200

    Update purposeErrorCodes

--- packages/commons/src/constants/purposeErrorCodes.ts
+++ packages/commons/src/constants/purposeErrorCodes.ts
@@ -1,19 +1,3 @@
 export const purposeErrorCodes = {
-  INVALID_DATE_FORMAT: {
-    code: "INVALID_DATE_FORMAT",
-    message: "Invalid value for field: date. Must be a valid date format.",
-  },
-  INVALID_PURPOSE_ID_FORMAT: {
-    code: "INVALID_PURPOSE_ID_FORMAT",
-    message:
-      "Invalid value for field: purpose_id. Must be a valid uuid format.",
-  },
-  INVALID_STATUS_CODE: {
-    code: "INVALID_STATUS_CODE",
-    message: "Invalid value for field: status. Must be a valid status code.",
-  },
-  INVALID_REQUESTS_COUNT: {
-    code: "INVALID_REQUESTS_COUNT",
-    message: "Invalid value for field: request_count. Must be a number.",
-  },
+  INVALID_DATE_FORMAT: "INVALID_DATE_FORMAT",
 };
