48c3c8c51d4fb9ca17b5c7c83ec70ddf954861b8
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-06-28 13:08:46 +0200

    Update model

--- packages/models/src/queue/messages.ts
+++ packages/models/src/queue/messages.ts
@@ -11,7 +11,7 @@ export type UpdateTracingStateDto = z.infer<typeof UpdateTracingStateDto>;
 
 export const SavePurposeErrorDto = z.object({
   tracingId: z.string().uuid(),
-  purposeId: z.string(),
+  purposeId: z.string().uuid(),
   version: z.number(),
   date: z.string(),
   errorCode: z.string(),
