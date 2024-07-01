16829494a9493bcd6b8fb6d32f28dbf64b4e3370
diego-lon <diego.longo@digitouch.it>
2024-07-01 17:06:17 +0200

    update purposeErrorCodes

--- packages/commons/src/constants/purposeErrorCodes.ts
+++ packages/commons/src/constants/purposeErrorCodes.ts
@@ -1,3 +1,14 @@
 export const purposeErrorCodes = {
-  INVALID_FORMAL_CHECK: "INVALID_FORMAL_CHECK",
+  INVALID_FORMAL_CHECK: {
+    code: "INVALID_FORMAL_CHECK",
+  },
+  INVALID_STATUS_CODE: {
+    code: "INVALID_STATUS_CODE",
+  },
+  INVALID_PURPOSE: {
+    code: "INVALID_PURPOSE",
+  },
+  INVALID_DATE: {
+    code: "INVALID_DATE",
+  },
 };
