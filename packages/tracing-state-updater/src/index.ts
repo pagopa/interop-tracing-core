f7a0710fc583c793aba15675b57fdd4ed58b6ca3
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-02 13:03:36 +0200

    Fix runConsumer handlers

--- packages/tracing-state-updater/src/index.ts
+++ packages/tracing-state-updater/src/index.ts
@@ -25,7 +25,7 @@ await SQS.runConsumer(
     queueUrl: config.sqsEndpointProcessingErrorQueue,
     consumerPollingTimeout: config.consumerPollingTimeout,
   },
-  processTracingStateMessage(OperationsService),
+  processPurposeErrorMessage(OperationsService),
 );
 
 await SQS.runConsumer(
@@ -34,5 +34,5 @@ await SQS.runConsumer(
     queueUrl: config.sqsEndpointEnricherStateQueue,
     consumerPollingTimeout: config.consumerPollingTimeout,
   },
-  processPurposeErrorMessage(OperationsService),
+  processTracingStateMessage(OperationsService),
 );
