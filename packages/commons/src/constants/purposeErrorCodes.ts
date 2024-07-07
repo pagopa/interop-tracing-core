04749d7a6e3d6ade1da46e3005112b95923d20de
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-07 14:15:37 +0200

    Added purposeErrorCodes

---
+++ packages/commons/src/constants/purposeErrorCodes.ts
@@ -0,0 +1,23 @@
+export const purposeErrorCodes = {
+  INVALID_FORMAL_CHECK: {
+    code: "INVALID_FORMAL_CHECK",
+  },
+  INVALID_STATUS_CODE: {
+    code: "INVALID_STATUS_CODE",
+  },
+  INVALID_PURPOSE: {
+    code: "INVALID_PURPOSE",
+  },
+  INVALID_DATE: {
+    code: "INVALID_DATE",
+  },
+  ESERVICE_NOT_ASSOCIATED: {
+    code: "ESERVICE_NOT_ASSOCIATED",
+  },
+  ESERVICE_NOT_FOUND: {
+    code: "ESERVICE_NOT_FOUND",
+  },
+  PURPOSE_NOT_FOUND: {
+    code: "PURPOSE_NOT_FOUND",
+  },
+};
