b989ee3a3364bf7c19e369b9d283fa3be144b467
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-07 16:39:34 +0200

    Update db

--- packages/tracing-operations/src/services/db/migration/V1__DDL.sql
+++ packages/tracing-operations/src/services/db/migration/V1__DDL.sql
@@ -9,8 +9,8 @@ CREATE TABLE tracing.tenants (
 );
 
 CREATE TABLE tracing.eservices (
-   eservice_id UUID NOT NULL,
-   producer_id UUID NOT NULL,
+    eservice_id UUID PRIMARY KEY,
+    producer_id UUID NOT NULL
 );
 
 CREATE TABLE tracing.purposes (
@@ -18,18 +18,21 @@ CREATE TABLE tracing.purposes (
     consumer_id UUID NOT NULL,
     eservice_id UUID NOT NULL,
     purpose_title VARCHAR(2048) NOT NULL,
-    FOREIGN KEY (consumer_id) REFERENCES tracing.tenants(id)
+    FOREIGN KEY (consumer_id) REFERENCES tracing.tenants(id),
+    FOREIGN KEY (eservice_id) REFERENCES tracing.eservices(eservice_id)
 );
 
 CREATE TABLE tracing.tracings (
     id UUID PRIMARY KEY,
     tenant_id UUID NOT NULL,
-    state VARCHAR(255) NOT NULL CHECK (state IN ('PENDING', 'COMPLETED', 'MISSING', 'ERROR')),
+    state VARCHAR(255) NOT NULL CHECK (
+        state IN ('PENDING', 'COMPLETED', 'MISSING', 'ERROR')
+    ),
     date TIMESTAMP NOT NULL,
     version INT NOT NULL,
     errors BOOLEAN NOT NULL DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_t TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (tenant_id) REFERENCES tracing.tenants(id)
 );
 
@@ -40,6 +43,7 @@ CREATE TABLE tracing.purposes_errors (
     purpose_id UUID NOT NULL,
     error_code VARCHAR(255) NOT NULL,
     message VARCHAR(2048) NOT NULL,
+    row_number INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (tracing_id) REFERENCES tracing.tracings(id),
     FOREIGN KEY (purpose_id) REFERENCES tracing.purposes(id)
