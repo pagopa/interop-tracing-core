3b4cd5852e83735b209dd57c9a7388e017fbe253
diego-lon <diego.longo@digitouch.it>
2024-07-08 12:25:30 +0200

    fixes deletion message on error

--- packages/tracing-processing/src/messageHandler.ts
+++ packages/tracing-processing/src/messageHandler.ts
@@ -1,12 +1,17 @@
 import { SQS } from "pagopa-interop-tracing-commons";
 import { ProcessingService } from "./services/processingService.js";
 import { decodeSqsMessage } from "./models/models.js";
+import { errorMapper } from "./utilities/errorMapper.js";
 
 export function processMessage(
   processingService: ProcessingService,
 ): (message: SQS.Message) => void {
   return async (message: SQS.Message) => {
-    const tracing = decodeSqsMessage(message);
-    processingService.processTracing(tracing);
+    try {
+      const tracing = decodeSqsMessage(message);
+      await processingService.processTracing(tracing);
+    } catch (e) {
+      throw errorMapper(e);
+    }
   };
 }
