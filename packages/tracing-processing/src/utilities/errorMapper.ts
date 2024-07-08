fb4a9d51700d1a92ab597700cf3a3eecd37f3ba9
diego-lon <diego.longo@digitouch.it>
2024-07-08 16:29:51 +0200

    rafactoring error mapper

--- packages/tracing-processing/src/utilities/errorMapper.ts
+++ packages/tracing-processing/src/utilities/errorMapper.ts
@@ -3,23 +3,16 @@ import {
   InternalError,
   genericInternalError,
 } from "pagopa-interop-tracing-models";
-import { match } from "ts-pattern";
+import { P, match } from "ts-pattern";
 import { ErrorCodes } from "../models/errors.js";
 
 type LocalErrorCodes = ErrorCodes | CommonErrorCodes;
 
-export const errorMapper = (error: unknown): InternalError<LocalErrorCodes> => {
-  if (error instanceof InternalError) {
-    const applicationError = error;
-    return match(applicationError.code)
-      .with("decodeSQSMessageError", () => applicationError)
-      .with("sendMessagePurposeError", () => applicationError)
-      .with("handlePurposesError", () => applicationError)
-      .with("readObjectBucketS3Error", () => applicationError)
-      .with("writeObjectBucketS3Error", () => applicationError)
-      .with("getEnrichedPurposeError", () => applicationError)
-      .otherwise(() => genericInternalError(`${error}`));
-  } else {
-    throw genericInternalError(`${error}`);
-  }
-};
+export const errorMapper = (error: unknown) =>
+  match<unknown, InternalError<LocalErrorCodes>>(error)
+    .with(P.instanceOf(InternalError<LocalErrorCodes>), (error) => {
+      throw error;
+    })
+    .otherwise((error: unknown) => {
+      throw genericInternalError(`${error}`);
+    });
