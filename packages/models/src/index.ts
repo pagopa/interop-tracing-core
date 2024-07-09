a36726ce12d065a82a88b01d911c9a529f3163b5
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-09 15:12:14 +0200

    removed unused notifcations models

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./errors.js";
 export * from "./brandedIds.js";
 export * from "./tracing/tracing.js";
-export * from "./notification/notification.js";
 export * from "./queue/messages.js";
