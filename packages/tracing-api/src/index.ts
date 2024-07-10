2a8892c92354bcf0649e98bf93fbdd6cc203ddc4
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-10 14:49:55 +0200

    Removed log

--- packages/tracing-api/src/index.ts
+++ packages/tracing-api/src/index.ts
@@ -5,5 +5,3 @@ import app from "./app.js";
 app.listen(config.port, config.host, () => {
   genericLogger.info(`listening on ${config.host}:${config.port}`);
 });
-
-console.log("lOG");
--- packages/tracing-operations/src/services/operationsService.ts
+++ packages/tracing-operations/src/services/operationsService.ts
@@ -88,11 +88,6 @@ export function operationsServiceBuilder(dbService: DBService) {
         state: tracingState.pending,
       });
 
-      await dbService.updateTracingVersion({
-        tracing_id: params.tracingId,
-        version: tracing.version + 1,
-      });
-
       return {
         tracingId: tracing.id,
         tenantId: tracing.tenant_id,
