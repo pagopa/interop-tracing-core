563f0789e182d9a679ff16850f12dc65812b45f7
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-10 16:09:21 +0200

    Update sql test

--- packages/tracing-processing/test/init-db.sql
+++ packages/tracing-processing/test/init-db.sql
@@ -10,21 +10,24 @@ CREATE TABLE IF NOT EXISTS tracing.tenants (
 
 CREATE TABLE IF NOT EXISTS tracing.eservices (
     eservice_id UUID PRIMARY KEY,
-    consumer_id VARCHAR,
-    producer_id VARCHAR
+    producer_id UUID NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tracing.purposes (
     id UUID PRIMARY KEY,
     consumer_id UUID NOT NULL,
     eservice_id UUID NOT NULL,
-    purpose_title VARCHAR(2048) NOT NULL
+    purpose_title VARCHAR(2048) NOT NULL,
+    FOREIGN KEY (consumer_id) REFERENCES tracing.tenants(id),
+    FOREIGN KEY (eservice_id) REFERENCES tracing.eservices(eservice_id)
 );
 
 CREATE TABLE IF NOT EXISTS tracing.tracings (
     id UUID PRIMARY KEY,
     tenant_id UUID NOT NULL,
-    state VARCHAR(255) NOT NULL CHECK (state IN ('PENDING', 'COMPLETED', 'MISSING', 'ERROR')),
+    state VARCHAR(255) NOT NULL CHECK (
+        state IN ('PENDING', 'COMPLETED', 'MISSING', 'ERROR')
+    ),
     date TIMESTAMP NOT NULL,
     version INT NOT NULL,
     errors BOOLEAN NOT NULL DEFAULT false,
@@ -40,7 +43,9 @@ CREATE TABLE IF NOT EXISTS tracing.purposes_errors (
     purpose_id UUID NOT NULL,
     error_code VARCHAR(255) NOT NULL,
     message VARCHAR(2048) NOT NULL,
+    row_number INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (tracing_id) REFERENCES tracing.tracings(id),
-    FOREIGN KEY (purpose_id) REFERENCES tracing.purposes(id)
+    FOREIGN KEY (purpose_id) REFERENCES tracing.purposes(id),
+    UNIQUE (tracing_id, purpose_id, version, row_number)
 );
