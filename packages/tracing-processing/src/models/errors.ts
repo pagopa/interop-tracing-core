8515451952686c418aac2c317cee3a9996197b97
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-10 12:24:57 +0200

    removed unused error handlePurposesError

--- packages/tracing-processing/src/models/errors.ts
+++ packages/tracing-processing/src/models/errors.ts
@@ -2,10 +2,9 @@ import { InternalError } from "pagopa-interop-tracing-models";
 export const errorCodes = {
   decodeSQSMessageError: "0601",
   sendMessagePurposeError: "0602",
-  handlePurposesError: "0603",
-  readObjectBucketS3Error: "0604",
-  writeObjectBucketS3Error: "0605",
-  getEnrichedPurposeError: "0606",
+  readObjectBucketS3Error: "0603",
+  writeObjectBucketS3Error: "0604",
+  getEnrichedPurposeError: "0605",
 } as const;
 
 export type ErrorCodes = keyof typeof errorCodes;
@@ -28,12 +27,6 @@ export function sendMessagePurposeError(
   });
 }
 
-export function handlePurposesError(detail: string): InternalError<ErrorCodes> {
-  return new InternalError({
-    detail: `${detail}`,
-    code: "handlePurposesError",
-  });
-}
 export function readObjectBucketS3Error(
   detail: string,
 ): InternalError<ErrorCodes> {
