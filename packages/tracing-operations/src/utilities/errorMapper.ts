230cbf55c4b9c36ca6c0ce26a956236ff54f0977
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-07-11 10:50:53 +0200

    updated code status tracingCannotBeUpdated to  HTTP_STATUS_CONFLICT

--- packages/tracing-operations/src/utilities/errorMapper.ts
+++ packages/tracing-operations/src/utilities/errorMapper.ts
@@ -9,11 +9,12 @@ const {
   HTTP_STATUS_INTERNAL_SERVER_ERROR,
   HTTP_STATUS_BAD_REQUEST,
   HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_CONFLICT,
 } = constants;
 
 export const errorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
     .with("tracingAlreadyExists", () => HTTP_STATUS_BAD_REQUEST)
     .with("tracingNotFound", () => HTTP_STATUS_NOT_FOUND)
-    .with("tracingCannotBeUpdated", () => HTTP_STATUS_BAD_REQUEST)
+    .with("tracingCannotBeUpdated", () => HTTP_STATUS_CONFLICT)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
