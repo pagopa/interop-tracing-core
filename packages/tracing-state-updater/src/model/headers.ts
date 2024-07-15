c050295f8e38211fa6cf24b8e8efc395b58d03e8
diego-lon <diego.longo@digitouch.it>
2024-07-15 17:02:55 +0200

    test and lint

--- packages/tracing-state-updater/src/model/headers.ts
+++ packages/tracing-state-updater/src/model/headers.ts
@@ -1,7 +1,7 @@
 import { CorrelationIdHeader } from "pagopa-interop-tracing-models";
 
 export const correlationIdToHeader = (
-  _correlationId: string,
+  correlationId: string,
 ): CorrelationIdHeader => ({
-  "X-Correlation-Id": "a10473fb-f11e-4a2d-9010-f771c50eb671",
+  "X-Correlation-Id": correlationId,
 });
