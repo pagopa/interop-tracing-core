6064f6386200ac26b8af6a9831684280837fd8d9
diego-lon <diego.longo@digitouch.it>
2024-09-20 15:43:43 +0200

    messagehandlerv2 fix

--- packages/purpose-event-consumer/src/handlers/messageHandlerV2.ts
+++ packages/purpose-event-consumer/src/handlers/messageHandlerV2.ts
@@ -1,4 +1,8 @@
-import { PurposeEventV2 } from "@pagopa/interop-outbound-models";
+import {
+  PurposeEventV2,
+  PurposeStateV2,
+  PurposeVersionV2,
+} from "@pagopa/interop-outbound-models";
 import { Logger, AppContext } from "pagopa-interop-tracing-commons";
 import {
   correlationIdToHeader,
@@ -7,6 +11,7 @@ import {
 import { P, match } from "ts-pattern";
 import { config } from "../utilities/config.js";
 import { OperationsService } from "../services/operationsService.js";
+import { kafkaInvalidVersion } from "../models/domain/errors.js";
 
 export async function handleMessageV2(
   event: PurposeEventV2,
@@ -20,9 +25,13 @@ export async function handleMessageV2(
         type: P.union("PurposeActivated"),
       },
       async (evt) => {
-        const { purpose } = evt.data;
-        if (!purpose) {
+        if (!evt.data.purpose) {
           throw kafkaMessageMissingData(config.kafkaTopic, event.type);
+        }
+        const { purpose } = evt.data;
+
+        if (hasPurposeVersionInAValidState(purpose.versions)) {
+          throw kafkaInvalidVersion();
         }
 
         await operationsService.savePurpose(
@@ -66,3 +75,46 @@ export async function handleMessageV2(
     )
     .exhaustive();
 }
+
+const validVersionInVersionsV2 = (
+  purposeVersions: PurposeVersionV2[],
+): { versionId: string; state: string } | undefined =>
+  match(purposeVersions)
+    .when(
+      (versions) =>
+        versions.some((version) => version.state === PurposeStateV2.ACTIVE),
+      () => getVersionBy(PurposeStateV2.ACTIVE, purposeVersions),
+    )
+    .when(
+      (versions) =>
+        versions.some((version) => version.state === PurposeStateV2.SUSPENDED),
+      () => getVersionBy(PurposeStateV2.SUSPENDED, purposeVersions),
+    )
+    .when(
+      (versions) =>
+        versions.some((version) => version.state === PurposeStateV2.ARCHIVED),
+      () => getVersionBy(PurposeStateV2.ARCHIVED, purposeVersions),
+    )
+    .otherwise(() => undefined);
+
+function getVersionBy(
+  purposeState: PurposeStateV2,
+  purposeVersions: PurposeVersionV2[],
+): {
+  versionId: string;
+  state: string;
+} {
+  return purposeVersions
+    .filter((version) => version.state === purposeState)
+    .reduce(
+      (obj, version) => {
+        const { id, state } = version;
+        return { ...obj, versionId: id, state: PurposeStateV2[state] };
+      },
+      {} as { versionId: string; state: string },
+    );
+}
+
+const hasPurposeVersionInAValidState = (
+  versions: PurposeVersionV2[],
+): boolean => !validVersionInVersionsV2(versions);
