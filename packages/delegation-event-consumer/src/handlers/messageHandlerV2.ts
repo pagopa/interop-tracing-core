ec033d524be16da39ac68a0befecfa3efdee47e3
diego-lon <diego.longo@digitouch.it>
2025-01-10 12:30:34 +0100

    Kafka delegations producer implementation (#75)

--- packages/delegation-event-consumer/src/handlers/messageHandlerV2.ts
+++ packages/delegation-event-consumer/src/handlers/messageHandlerV2.ts
@@ -49,7 +49,7 @@ export async function handleMessageV2(
           );
         } else {
           logger.info(
-            `Skip event: Delegation kind - ${delegation.kind} - is not relevant`,
+            `Skip event: Delegation ${delegation.id} with kind - ${delegation.kind} - is not relevant`,
           );
         }
       },
