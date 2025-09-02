d1df17f9c77b204a055a59216238414c1981171c
diego-lon <diego.longo@digitouch.it>
2025-09-02 12:55:50 +0200

    PIN-7483: CORS removed from api package (#98)

--- packages/api/src/utilities/config.ts
+++ packages/api/src/utilities/config.ts
@@ -16,13 +16,11 @@ const apiConfig = HTTPServerConfig.and(LoggerConfig)
       .object({
         APPLICATION_NAME: z.string(),
         API_OPERATIONS_BASEURL: z.string(),
-        CORS_ORIGIN_ALLOWED: z.string().optional(),
         STORAGE_PATH_NAME: z.string(),
       })
       .transform((c) => ({
         applicationName: c.APPLICATION_NAME,
         operationsBaseUrl: c.API_OPERATIONS_BASEURL,
-        corsOriginAllowed: c.CORS_ORIGIN_ALLOWED,
         storagePathName: c.STORAGE_PATH_NAME,
       })),
   );
