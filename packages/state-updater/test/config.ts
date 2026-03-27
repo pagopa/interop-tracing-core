27c4d2826b0ff8bd0007f009ec41ec7040deba28
Eduardo Mihalache <eduardo.mihalache@digitouch.it>
2026-03-27 18:37:48 +0100

    chore: rename ref

--- packages/state-updater/test/config.ts
+++ packages/state-updater/test/config.ts
@@ -5,12 +5,12 @@ import { TracingStateUpdateConfig } from "../src/utilities/config.js";
 
 export const TEST_POSTGRES_DB_PORT = 5432;
 export const TEST_MINIO_PORT = 9000;
-export const TEST_POSTGRES_DB_WITH_AWS_S3_IMAGE = "postgres:14";
+export const TEST_POSTGRES_DB_IMAGE = "postgres:14";
 export const TEST_MINIO_IMAGE =
   "quay.io/minio/minio:RELEASE.2024-02-06T21-36-22Z";
 
 export const postgreSQLContainer = (config: DbConfig): GenericContainer =>
-  new GenericContainer(TEST_POSTGRES_DB_WITH_AWS_S3_IMAGE)
+  new GenericContainer(TEST_POSTGRES_DB_IMAGE)
     .withEnvironment({
       POSTGRES_DB: config.dbName,
       POSTGRES_USER: config.dbUsername,
@@ -20,14 +20,14 @@ export const postgreSQLContainer = (config: DbConfig): GenericContainer =>
       {
         source: resolve(
           __dirname,
-          "../../../docker/tracing-store-db/init-db.sql",
+          "../../../docker/tracing-store-db/init-db.sql"
         ),
         target: "/docker-entrypoint-initdb.d/01-init-schema.sql",
       },
       {
         source: resolve(
           __dirname,
-          "../../../docker/tracing-store-db/init-db-seed.sql",
+          "../../../docker/tracing-store-db/init-db-seed.sql"
         ),
         target: "/docker-entrypoint-initdb.d/02-init-seed.sql",
       },
@@ -35,7 +35,7 @@ export const postgreSQLContainer = (config: DbConfig): GenericContainer =>
     .withExposedPorts(TEST_POSTGRES_DB_PORT);
 
 export const minioContainer = (
-  config: TracingStateUpdateConfig,
+  config: TracingStateUpdateConfig
 ): GenericContainer =>
   new GenericContainer(TEST_MINIO_IMAGE)
     .withEnvironment({
