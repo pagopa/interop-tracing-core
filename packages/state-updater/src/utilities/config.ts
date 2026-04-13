57be94255876ccb7da6a0be68d06440253337235
Eduardo <eduardo.mihalache@digitouch.it>
2026-04-13 14:47:28 +0200

    chore(PIN-9570): align env config naming state-updater like processing (#118)

--- packages/state-updater/src/utilities/config.ts
+++ packages/state-updater/src/utilities/config.ts
@@ -15,7 +15,7 @@ const tracingStateUpdateronfig = AWSConfig.and(ConsumerConfig)
     z
       .object({
         APPLICATION_NAME: z.string(),
-        SQS_ENDPOINT_PROCESSING_RESULTS_QUEUE: z.string(),
+        SQS_PROCESSING_RESULTS_ENDPOINT: z.string(),
         SQS_ENDPOINT: z.string().nullish(),
         S3_TRACING_ERRORS_BUCKET_NAME: z.string(),
         AWS_ACCESS_KEY_ID: z.string().nullish(),
@@ -24,8 +24,7 @@ const tracingStateUpdateronfig = AWSConfig.and(ConsumerConfig)
       })
       .transform((c) => ({
         applicationName: c.APPLICATION_NAME,
-        sqsEndpointProcessingResultsQueue:
-          c.SQS_ENDPOINT_PROCESSING_RESULTS_QUEUE,
+        sqsProcessingResultsEndpoint: c.SQS_PROCESSING_RESULTS_ENDPOINT,
         sqsEndpoint: c.SQS_ENDPOINT,
         bucketTracingErrorsS3Name: c.S3_TRACING_ERRORS_BUCKET_NAME,
         awsAccessKeyId: c.AWS_ACCESS_KEY_ID,
